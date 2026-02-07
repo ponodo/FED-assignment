4b6d975d893498980b5b1f42d09d6d52d1488315
stevejobsdiedofligma <jdenongcool@gmail.com>
2026-02-07 18:05:52 +0800

    rental agreement firebase

---
+++ js/stallmanageFB.js
@@ -0,0 +1,15 @@
+// firebase.js
+// Firebase config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
